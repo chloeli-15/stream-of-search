1528b78c26ba68413d462693e68f23541a295d27
Melina Jingting Laimon <melinajingting@gmail.com>
2025-03-18 11:10:11 +0000

    sources as additional columns in hf dataset

--- scripts/convert_to_sftrainer_format.py
+++ scripts/convert_to_sftrainer_format.py
@@ -8,41 +8,32 @@ def convert_to_sftrainer_format(input_json_path, output_dir):
     with open(input_json_path, "r") as f:
         data = json.load(f)
     
-    formatted_data_search = []
-    formatted_data_optimal = []
+    formatted_data = []
     
     for entry in tqdm(data, desc=f"Processing {os.path.basename(input_json_path)}"):
         user_prompt = f"Make {entry['target']} with the numbers {entry['nums']} using standard arithmetic operations."
 
-        formatted_entry_search = {
-            "messages": [
+        entry["messages_sos_explained_v1"] = [
                 {"role": "user", "content": user_prompt},
-                {"role": "assistant", "content": entry['search_path']}
+                {"role": "assistant", "content": entry.pop('search_path')}
             ]
-        }
         
-        formatted_entry_optimal = {
-            "messages": [
+        entry["messages_optimal"] = [
                 {"role": "user", "content": user_prompt}, 
-                {"role": "assistant", "content": entry['optimal_path']}
+                {"role": "assistant", "content": entry.pop('optimal_path')}
             ]
-        }
         
-        formatted_data_search.append(formatted_entry_search)
-        formatted_data_optimal.append(formatted_entry_optimal)
+        formatted_data.append(entry)
     
     # Create the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
     
     base_filename = os.path.splitext(os.path.basename(input_json_path))[0]
     output_json_path_search = os.path.join(output_dir, f"{base_filename}_search.json")
-    output_json_path_optimal = os.path.join(output_dir, f"{base_filename}_optimal.json")
     
     with open(output_json_path_search, "w+") as f:
-        json.dump(formatted_data_search, f, indent=4)
+        json.dump(formatted_data, f, indent=4)
     
-    with open(output_json_path_optimal, "w+") as f:
-        json.dump(formatted_data_optimal, f, indent=4)
 
 def convert_directory(input_dir, output_dir):
     for filename in os.listdir(input_dir):
