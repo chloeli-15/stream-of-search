1a2af87fbcb712276282bf008870ad1a7d707961
Melina Jingting Laimon <melinajingting@gmail.com>
2025-04-03 10:16:56 +0100

    token lengths visuals

--- scripts/gen_countdown_hard.sh
+++
@@ -1,1 +0,0 @@
-python src/countdown_generate.py --seed 4 --data_dir data/b4_3_random/ --min_range 5 --start_range 5 --num_samples 1000
---
+++ scripts/gen_countdown_ood.sh
@@ -0,0 +1,2 @@
+python src/countdown_generate_ood.py --seed 4 --data_dir data/countdown_3_5num/ --min_range 5 --start_range 5 --num_samples 1000
+python src/countdown_generate_ood.py --seed 4 --data_dir data/countdown_3_5num/ --min_range 3 --start_range 3 --num_samples 1000
